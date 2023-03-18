79100ffd530538b528cbb16d70eabdcde28fd08d
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2023-03-18 14:03:32 +0500

    changes

---
+++ resources/lang/de/content.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'main_heading' => 'Halal Certification Services Germany',
+    'main_subheading'=>'Halal Certification Services (HCS) (EU) is an experienced and world-wide recognized and respected certification company providing assessment, auditing and training services.',
+    'partner_message'=>'Trusted by more than 400+ companies worldwide',
+    'section_heading'=>'Halal Certification Services',
+    'section_message_1'=>'Halal Certification Services (HCS) (EU) is an experienced and world-wide recognized and respected certification company providing assessment, auditing and training services.',
+    'section_message_2'=>'HCS(EU) based in Germany is assisting ingredients, food products, pharmaceutical and cosmetic manufacturers to cater the halal market needs.',
+    'section_message_3'=>'HCS(EU) is a member of World Halal Food Council and is officially recognized from renowned bodies from around the world such as MUI (Indonesia), JAKIM (Malaysia) and MUIS (Singapore).',
+    'about_section_title'=>'About us',
+    'about_section_heading'=>'We would like to thank you for your intention to know more about us and our organization.',
+    'about_section_message_1'=>'Halal Certification services (EU) GmbH was initiated by Swiss entity to expand its services into Europe. The office based in south Germany caters to its German Clients and enables a presence in the EU market. HCS (EU) can serve the industry professionally through its own experienced auditors.',
+    'about_section_message_2'=>'Meanwhile Halal Certification Services (EU) is a renowned and respected body all over the World. HCS (EU) is continuously working towards awareness of Halal and is striving to increase the diversity of Halal certified products.',
+    'history_title'=>'Our History',
+    'history_message_1'=>"Halal Certification Services (EU) GmbH is a young enterprise. It was initiated in 2015 and hopes to expand it's importance gradually and find it's place in the emerging halal market place.",
+    'history_message_2'=>"HCS (EU) is meant to be a focal point for the German industry striving to cater to halal market needs.",
+    'mission_title'=>'Our Mission',
+    'mission_message_1'=>"HCS (EU) aims for the highest standards with regard to halal products as governed by the teachings of the Qur'an and the Sunnah of our beloved prophet Mohammed (S.A.W).",
+    'mission_message_2'=>"As a result of increasing standardization and growing demand for an independent, reliable, and internationally recognized certification system, HCS (EU) has designed and implemented a halal auditing system which shall oversee the process all the way from the source of production to the point of export form Europe.",
+    'mission_message_3'=>'HCS (EU) ensures halal compliance of an industry and provides information, advice, and supervision to help the interested parties achieve their aim of producing halal products.',
+    'mission_message_4'=>"Where standards are not met, certification and validation is refused to that party. But it is important to emphasize that the aim of HCS, is to help interested parties seeking halal certification to become fully aware of and achieve a position where they are capable of producing and manufacturing authentic halal products.",
+    'halal_explained_title'=>'Halal Explained',
+];
+
+
+
+
